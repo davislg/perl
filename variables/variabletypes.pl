57ff547c9a62987394b93ab1209740c105f93ff0
Davis <lawrence.davis@gmail.com>
2012-04-27 15:43:52 -0600

    Updated Variable comments and formatting. Added functions for individual types.

--- variables/variabletypes.pl
+++ variables/variabletypes.pl
@@ -2,110 +2,194 @@
 use strict;
 use warnings;
 
-print "\n";
-print "# --------------\n";
-print "# Variable Types\n";
-print "# --------------\n";
+# Variable Types
+printHeader("Variable Types");
 
-my $undef = undef; # null representive
-print $undef . "\n"; # error
+# Null Types
+printNullTypes();
 
-# implicit undef:
-my $undef2;
-print $undef2 . "\n"; # same error as above
+# Scalar Types
+printScalarTypes();
 
-my $num = 4040.5;
-print $num . "\n"; # "4040.5"
+# Weakly Typed
+printWeaklyTyped();
 
-my $string = "world";
-print $string . "\n"; # "world"
+# Array Types
+printArrayTypes();
 
-# String concatenation
-print "Hello ".$string."\n"; # "Hello world"
+# Hash Types
+printHashTypes();
 
-# String concatenation through passing multiple arguements
-print "Hello ",$string,"\n"; # "Hello world"
+# Square Brackets [] vs Braces {}
+printSquareBracketsvsBraces();
 
-print "\n";
-print "# -----------------\n";
-print "# Weakly Typed Test\n";
-print "# -----------------\n";
-my $str1 = "4G";
-my $str2 = "4H";
+# ********************************
+# Function Definitions
+# ********************************
 
-print $str1 .  $str2 ."\n"; # "4G4H"
-print $str1 +  $str2 ."\n"; # "8" with two warnings
-print $str1 eq $str2 ."\n"; # "" (empty string, i.e. false)
-print $str1 == $str2 ."\n"; # "1" with NO WARNING!
+# ---------------
+# Print Header 
+# ---------------
+sub printHeader {
+  my $length = length($_[0]); 
 
-print "\n";
-print "# ------\n";
-print "# Arrays\n";
-print "# ------\n";
+  print "\n";
+  # Header top line
+  print "# ";
+  for (my $i=0; $i < $length; $i++) {
+    print "-";
+  }
+  print "\n";
 
-my @array = (
-  "print ",
-  "these ",
-  "strings ",
-  "out ",
-  "for ",
-  "me ", # trailing comma is okay/valid
-);
-print $array[0]; # "print"
-print $array[1]; # "these"
-print $array[2]; # "strings"
-print $array[3]; # "out"
-print $array[4]; # "for"
-print $array[5]; # "me"
-#print $array[6]; # warning
-print "\n";
+  # Header Title
+  print "# ".$_[0]."\n";
 
-# Working backwards through the array
-print $array[-1]; # "me"
-print $array[-2]; # "for"
-print $array[-3]; # "out"
-print $array[-4]; # "strings"
-print $array[-5]; # "these"
-print $array[-6]; # "print"
-#print $array[-7]; # warning
-print "\n";
+  # Header bottom line
+  print "# ";
+  for (my $i=0; $i < $length; $i++) {
+    print "-";
+  }
+  print "\n";
+}
 
-# Array Length
-print "This array has ", (scalar @array), " elements", "\n"; # "This array has 6 elements"
-print "The last populated index is ", $#array,"\n";        # "The last populated index is 5"
+# ------------------
+# Print Null Values
+# ------------------
+sub printNullTypes {
+  printHeader("Null Types");
 
-# Interpolated strings
-print "@array\n";
+  print("NULL (undef) type [Explicit]:\n");
+  my $undef = undef;                                # null representive
+  print("\t$undef\n");                              # error
 
-print "\n";
-print "# --------------\n";
-print "# Hash Variables\n";
-print "# --------------\n";
-my %scientists = (
-  "Newton"   => "Isaac",
-  "Einstein" => "Albert",
-  "Darwin"   => "Charles",
-);
-print $scientists{"Newton"}."\n";   # "Isaac"
-print $scientists{"Einstein"}."\n"; # "Albert"
-print $scientists{"Darwin"}."\n";   # "Charles"
-#print $scientists{"Dyson"}."\n";    # runtime error - key not set
+  print("NULL (undef) type [Implicit]:\n");
+  my $undef2;                                       # null representive (same as above)
+  print("\t$undef2\n");                             # same error as above
+}
 
-# Convert Hash to Array
-my @scientistArray = %scientists;
-print "@scientistArray\n";
+# --------------------
+# Print Scalar Types 
+# --------------------
+sub printScalarTypes {
+  printHeader("Scalar Types");
 
-print "\n";
-print "# -------------------------\n";
-print "# Square Brackets vs Braces\n";
-print "# -------------------------\n";
-my $data = "orange";
-my @data = ("purple");
-my %data = ( "0" => "blue");
+  # Simple
+  my $localScalar = "This is a scalar variable.";
+  print("\$localScalar: $localScalar\n");
 
-print $data." ";      # "orange"
-print $data[0]." ";   # "purple"
-print $data["0"]." "; # "purple"
-print $data{0}." ";   # "blue"
-print $data{"0"}." "; # "blue"
-print "\n";
+  # Number
+  my $num = 4040.5;
+  print("$num\n");                                  # 4040.5
+
+  # Concatenation using dot (.)
+  my $world = "World";
+  print("Hello ".$world."\n");                      # Hello world
+
+  # Concatenation passing multiple arguements
+  print("Hello ",$world,"\n");                      # Hello world
+}
+
+# --------------------
+# Weakly Typed
+# --------------------
+sub printWeaklyTyped {
+  printHeader("Weakly Typed");
+
+  my $str1 = "4G";
+  my $str2 = "4H";
+
+  print($str1 .  $str2 ."\n");                      # "4G4H"
+  print($str1 +  $str2 ."\n");                      # "8" with two warnings
+  print($str1 eq $str2 ."\n");                      # "" (empty string, i.e. false)
+  print($str1 == $str2 ."\n");                      # "1" with NO WARNING!
+  print("\n");
+}
+
+# -------------------
+# Array Types
+# -------------------
+sub printArrayTypes {
+  printHeader("Array Types");
+  
+  my @array = (
+    "print",
+    "these",
+    "strings",
+    "out",
+    "for",
+    "me",                                           # trailing comma is okay/valid
+  );
+  
+  # Print array elements
+  print($array[0]);                                 # print
+  print($array[1]);                                 # these
+  print($array[2]);                                 # strings
+  print($array[3]);                                 # out
+  print($array[4]);                                 # for
+  print($array[5]);                                 # me
+  #print($array[6]);                                # warning
+  print("\n");
+
+  # Working backwards through the array
+  print($array[-1]);                                # me
+  print($array[-2]);                                # for
+  print($array[-3]);                                # out
+  print($array[-4]);                                # strings
+  print($array[-5]);                                # these
+  print($array[-6]);                                # print
+  #print($array[-7]);                               # warning
+  print("\n");
+  
+  # Array Length
+  print("This array has ", 
+    (scalar @array), " elements", 
+    "\n");                                          # This array has 6 elements
+  print("The last populated index is ", 
+    $#array,
+    "\n");                                          # The last populated index is 5
+
+  # Interpolated strings
+  print("@array\n");
+}
+
+# --------------------
+# Hash Types
+# --------------------
+sub printHashTypes() {
+  printHeader("Hash Types");
+  
+  my %scientists = (
+    "Newton"   => "Isaac",
+    "Einstein" => "Albert",
+    "Darwin"   => "Charles",
+  );
+
+  print($scientists{"Newton"}."\n");                # Isaac
+  print($scientists{"Einstein"}."\n");              # Albert
+  print($scientists{"Darwin"}."\n");                # Charles
+  #print($scientists{"Dyson"}."\n");                # runtime error - key not set
+
+  # Convert Hash to Array
+  my @scientistArray = %scientists;
+  print("@scientistArray\n");
+}
+
+# -------------------------------
+# Square Brackets [] vs Braces {}
+# -------------------------------
+sub printSquareBracketsvsBraces {
+  printHeader("Square Brackets [] vs Braces {}");
+
+  my $data = "orange";
+  my @data = ("purple");
+  my %data = ( "0" => "blue");
+
+  print($data." ");                                 # orange
+  print($data[0]." ");                              # purple
+  print($data["0"]." ");                            # purple
+  print($data{0}." ");                              # blue
+  print($data{"0"}." ");                            # blue
+  print("\n");
+}
+
+
