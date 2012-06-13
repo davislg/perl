f1ad30fcee02fa9412ded883afed6f852aec8119
Davis <lawrence.davis@gmail.com>
2012-06-13 12:59:15 -0700

    Updated Title

--- html/helloworld/helloworld.pl
+++ html/helloworld/helloworld.pl
@@ -4,7 +4,7 @@ use warnings;
 
 print "Content-type: text/html\n\n";
 print "<html><head>";
-print "<title>CGI Test</title>";
+print "<title>Hello World!</title>";
 print "</head>";
 print "<body><h3>Hello World!</h3>";
 print "</body></html>"; 
--- html/helloworld/hey.asp
+++ html/helloworld/hey.asp
@@ -4,7 +4,7 @@ use warnings;
 
 print "Content-type: text/html\n\n";
 print "<html><head>";
-print "<title>CGI Test</title>";
+print "<title>Hey World!</title>";
 print "</head>";
 print "<body><h3>Hey World!</h3>";
 print "</body></html>"; 
